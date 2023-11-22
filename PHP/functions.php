c406de45d7567b615ab00c7ec72d6fc3735dd240
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-11-22 22:23:37 +0100

    added function in php to get random movies

--- PHP/functions.php
+++ PHP/functions.php
@@ -7,9 +7,28 @@ function sendJSON($message, $http_code = 200)
     exit();
 }
 
+
 function putInUsersJSON($newData){
     $filename = __DIR__."/../DATA/users.json";
     file_put_contents($filename, json_encode($newData, JSON_PRETTY_PRINT));
+}
+
+function getRandomMovies($number){
+    $filename = __DIR__."/../DATA/movies.json";
+
+    if (file_exists($filename)) {
+        $movies = json_decode(file_get_contents($filename), true);
+    } else {
+        $movies = [];
+    }
+
+    $randomMovies = [];
+
+    foreach(array_rand($movies, $number) as $index){
+        $randomMovie[] = $movies[$index];
+    }
+
+    return $randomMovies;
 }
 
 ?>
