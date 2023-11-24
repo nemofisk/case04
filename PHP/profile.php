94e44b577118219d97e9a62f598db3a7c876f220
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-11-24 18:22:02 +0100

    profile php update

--- PHP/profile.php
+++ PHP/profile.php
@@ -11,17 +11,24 @@ function profile($users, $received_data){
             if($user["username"] === $username){
                 $user["username"] = $newUsername;
 
-                $
-
+                putInUsersJSON($users);
+                sendJSON(["message" => "Username changed successfully!"], 200);
             }
         }
     }
 
     if($subAction === "changePassword"){
+        $newPassword = $received_data["newPassword"];
 
+        foreach($users as $user){
+            if($user["username"] === $username){
+                $user["password"] = $newPassword;
+
+                putInUsersJSON($users);
+                sendJSON(["message" => "Password changed successfully!"], 200);
+            }
+        }
     }
-
-    if($subAction === "")
 }
 
 ?>
