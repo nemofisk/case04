014c88a9d7cc7f30e45ade220193e75288b4c053
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-12-01 15:33:34 +0100

    game object structure, invites in user object, new function in multiplayer php

---
+++ PHP/multiplayer.php
@@ -0,0 +1,10 @@
+<?php 
+
+ini_set("display_errors", 1);
+
+function multiplayer($users, $received_data, $games){
+    $subAction = $received_data["subAction"];
+    $username = $received_data["username"];
+}
+
+?>
