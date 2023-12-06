d61e9329708e4567a4f06a1bc9b72c9c1b0c5d40
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2023-12-06 15:23:21 +0100

    multiplayer

---
+++ JS/multiplayer.js
@@ -0,0 +1,43 @@
+function chooseCatagoryMultiplayer(params) {
+    genreArray = [
+        "Crime",
+        "Drama",
+        "History",
+        "Action",
+        "Romance",
+        "Adventure",
+        "Fantasy",
+        "Sci-Fi",
+        "Thriller",
+        "Animation",
+        "Comedy",
+        "Horror",
+    ];
+
+
+
+    document.querySelector("main").innerHTML = `
+    <header id="menu">
+
+        <div class="profile">
+            <div id="profilePic"></div>
+            <p>TheMovieStar</p>
+        </div>
+    </header>
+    
+
+    <div>Choose a category!</div>
+    <div id="catergoryMenu"></div>
+    <div> i dont want to choose a category</div>
+        
+
+    </div>`
+    let genresCategorty = document.querySelector("#catergoryMenu");
+
+    for (let i = 0; i < genreArray.length; i++) {
+        let div = document.createElement("div");
+        div.textContent = genreArray[i];
+        genresCategorty.appendChild(div)
+        div.addEventListener("click", chooseGenre)
+    }
+}
