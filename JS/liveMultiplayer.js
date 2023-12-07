0c8440d6256c900370a3cf22e348655fd89c52a2
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-12-07 13:46:55 +0100

    started live MP

---
+++ JS/liveMultiplayer.js
@@ -0,0 +1,13 @@
+"use strict";
+
+async function joinGame() {
+    const main = document.querySelector("main");
+
+    main.innerHTML = `
+        
+    `
+}
+
+async function fetchGameInfo() {
+
+}
